3f432b5ab682e016fd521750953d5e4019966258
Pablo Gonzalez <pgonza25@arm.csc.calpoly.edu>
2022-02-21 23:51:21 -0500

    more updates to main

--- main.s
+++ main.s
@@ -25,9 +25,10 @@ calculate:
     // Print prompt and scan for second number
     ldr w0, =number2
     bl printf
+    ldr w0, =scanint
     mov x1, sp
     bl scanf
-    ldr x3, [sp]
+    ldr x4, [sp]
 
     // Print prompt and scan for operation
     ldr w0, =operation
@@ -35,8 +36,38 @@ calculate:
     ldr w0, =scanchar
     mov x1, sp //Save sp in x1
     bl scanf  //Scan for operator
+    ldrb x5, [sp]
 
+    //do checks to see if mult, add, or sub
+    ldr x6, =multiply
+    cmp x6, 
+    b.eq mult
 
+    ldr x6, =subtract
+    cmp x6,
+    b.eq sub
+
+    ldr x6, =addition
+    cmp x6,
+    b.eq add
+
+mult:
+    mov x0, x3
+    mov x1, x4
+    bl intmul
+    b loop
+
+add:
+    mov x0, x3
+    mov x1, x4
+    bl intadd
+    b loop
+
+sub:
+    mov x0, x3
+    mov x1, x4
+    bl intsub
+    b  loop
 
     // You'll need to scan characters for the operation and to determine
     // if the program should repeat.
@@ -72,5 +103,5 @@ multiply:
     .byte   '*'
 subtract:
     .byte   '-'
-add:
+addition:
     .byte   '+'
